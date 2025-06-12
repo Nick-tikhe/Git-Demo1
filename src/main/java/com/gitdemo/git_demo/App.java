78c146aa25746a4350b1116c2eb5e9339f2a85c4
Nick-tikhe <163338725+Nick-tikhe@users.noreply.github.com>
2025-06-12 23:38:55 +0530

    data added after giving check condition

---
+++ src/main/java/com/gitdemo/git_demo/App.java
@@ -0,0 +1,19 @@
+package com.gitdemo.git_demo;
+
+/**
+ * Hello world!
+ */
+public class App {
+	  
+	public void addData(String data) {
+		if(data.equals("nick")) {
+			System.out.println("Welcome Nick");
+		}
+		System.out.println("data = "+data);
+	}
+	
+    public static void main(String[] args) {
+    	
+        System.out.println("Hello World!");
+    }
+}
